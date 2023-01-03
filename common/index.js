89320cc62103278bb9fc0e281daab7183069a946
orriy <jeonghuigim63@gmail.com>
2023-01-03 10:46:47 +0900

    first commit

---
+++ common/index.js
@@ -0,0 +1,115 @@
+// banner
+window.addEventListener("scroll", function () {
+    const scrollTop = document.documentElement.scrollTop;
+  
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+  
+    const scrollWidth = (scrollTop / height) * 100;
+  
+    const $img = document.querySelector(".img");
+    $img.style.cssText = `
+        width :${60 + scrollWidth * 5}vw;
+        height :${70 + scrollWidth * 5}vh
+      `;
+  });
+  
+  // sec2
+  Vue.config.devtools = true;
+  
+  Vue.component("card", {
+    template: `
+      <div class="card-wrap"
+        @mousemove="handleMouseMove"
+        @mouseenter="handleMouseEnter"
+        @mouseleave="handleMouseLeave"
+        ref="card">
+        <div class="card"
+          :style="cardStyle">
+          <div class="card-bg" :style="[cardBgTransform, cardBgImage]"></div>
+          <div class="card-info">
+            <slot name="header"></slot>
+            <slot name="content"></slot>
+          </div>
+        </div>
+      </div>`,
+    mounted() {
+      this.width = this.$refs.card.offsetWidth;
+      this.height = this.$refs.card.offsetHeight;
+    },
+    props: ["dataImage"],
+    data: () => ({
+      width: 0,
+      height: 0,
+      mouseX: 0,
+      mouseY: 0,
+      mouseLeaveDelay: null,
+    }),
+  
+    computed: {
+      mousePX() {
+        return this.mouseX / this.width;
+      },
+      mousePY() {
+        return this.mouseY / this.height;
+      },
+      cardStyle() {
+        const rX = this.mousePX * 30;
+        const rY = this.mousePY * -30;
+        return {
+          transform: `rotateY(${rX}deg) rotateX(${rY}deg)`,
+        };
+      },
+      cardBgTransform() {
+        const tX = this.mousePX * -40;
+        const tY = this.mousePY * -40;
+        return {
+          transform: `translateX(${tX}px) translateY(${tY}px)`,
+        };
+      },
+      cardBgImage() {
+        return {
+          backgroundImage: `url(${this.dataImage})`,
+        };
+      },
+    },
+  
+    methods: {
+      handleMouseMove(e) {
+        this.mouseX = e.pageX - this.$refs.card.offsetLeft - this.width / 2;
+        this.mouseY = e.pageY - this.$refs.card.offsetTop - this.height / 2;
+      },
+      handleMouseEnter() {
+        clearTimeout(this.mouseLeaveDelay);
+      },
+      handleMouseLeave() {
+        this.mouseLeaveDelay = setTimeout(() => {
+          this.mouseX = 0;
+          this.mouseY = 0;
+        }, 1000);
+      },
+    },
+  });
+  
+  const app = new Vue({
+    el: "#app",
+  });
+  
+  // sec3
+  let tabHeader = document.getElementsByClassName("tab-header")[0];
+  let tabIndicator = document.getElementsByClassName("tab-indicator")[0];
+  let tabBody = document.getElementsByClassName("tab-body")[0];
+  
+  let tabsPane = tabHeader.getElementsByTagName("div");
+  
+  for (let i = 0; i < tabsPane.length; i++) {
+    tabsPane[i].addEventListener("click", function () {
+      tabHeader.getElementsByClassName("active")[0].classList.remove("active");
+      tabsPane[i].classList.add("active");
+      tabBody.getElementsByClassName("active")[0].classList.remove("active");
+      tabBody.getElementsByTagName("div")[i].classList.add("active");
+  
+      tabIndicator.style.left = `calc(calc(100% / 4) * ${i})`;
+    });
+  }
